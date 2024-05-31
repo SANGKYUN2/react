53a6ac1a06d9ef87a5e283b5ad80678a200cf26a
SANGKYUN2 <gr001231@naver.com>
2024-05-31 16:44:34 +0900

    채팅하기

---
+++ ex04/src/components/post/ReadPage.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { Card, Row, Col, Button } from 'react-bootstrap'
+import { app } from '../../firebaseInit'
+import { getFirestore, doc, getDoc, deleteDoc } from 'firebase/firestore'
+import ListPage from '../comment/ListPage'
+
+const ReadPage = () => {
+  const navi = useNavigate();
+  const [post, setPost] = useState('');
+  const db = getFirestore(app);
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  const { email, body, title, date } = post;//비구조할당
+
+  const callAPI = async () => {
+    setLoading(true);
+    const res = await getDoc(doc(db, 'posts', id));
+    console.log(res.data());
+    setPost(res.data());
+    setLoading(false);
+  }
+  useEffect(() => {
+    callAPI();
+  }, [])
+
+  const onClickDelete = async () => {
+    if (!window.confirm(`${id}번 게시글을 삭제하실래요`)) return;
+    //게시글삭제
+    setLoading(true);
+    await deleteDoc(doc(db, `posts/${id}`));
+    setLoading(false);
+    navi('/post/list');
+  }
+
+  if (loading) return <h1 className='text-center my-5'>로딩중...</h1>
+  return (
+    <>
+      <Row className='justify-content-center my-5' >
+        <h1 className='text-center mb-5'>게시글정보</h1>
+        <Col xs={10} md={8} lg={7}>
+
+          {email === sessionStorage.getItem("email") &&
+            <div className='my-2 text-end'>
+              <Button onClick={() => navi(`/post/update/${id}`)}
+                className='me-2' variant='outline-primary'>수정</Button>
+              <Button variant='outline-danger' onClick={onClickDelete}>삭제</Button>
+            </div>
+          }
+
+          <Card>
+            <Card.Body>
+              <h5>{title}</h5>
+              <hr />
+              <div style={{ whiteSpace: "pre-wrap" }}>{body}</div>
+            </Card.Body>
+            <Card.Footer className='text-muted text-end'>
+              <b>Posted</b> {date} <b>by</b> {email}
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+      {/****************댓글목록***************/}
+      <ListPage id={id}/>
+    </>
+  )
+}
+
+export default ReadPage
---
+++ ex04/src/components/user/MyPage.jsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react'
+import {Row, Col, Card, Form, InputGroup, Button} from 'react-bootstrap'
+import { app } from '../../firebaseInit'
+import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore'
+import ModalAddress from './ModalAddress'
+import ModalPhoto from './ModalPhoto'
+
+const MyPage = () => {
+  const [loading, setLoading] = useState(false);
+  const db=getFirestore(app);
+  const email=sessionStorage.getItem('email');
+  const uid=sessionStorage.getItem('uid');
+  const [form, setForm] = useState({
+    email:email,
+    name: '김스타',
+    phone: '010-1522-3232',
+    address1: '서울 디지털로9길 23',
+    address2: '스타벅스 커피 마리오 아울렛점'
+  });
+
+  const {name, phone, address1, address2} = form; //비구조할당
+  const onChangeForm = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]:e.target.value
+    })
+  }
+
+  const onSubmit = async(e) => {
+    e.preventDefault();
+    if(name===""){
+      alert("이름입력하세요!");
+      return;
+    }
+    if(!window.confirm("변경내용을 저장하실래요?")) return;
+    //console.log(form);
+    //사용자정보저장
+    setLoading(true);
+    await setDoc(doc(db, 'users', uid), form);
+    setLoading(false);
+    alert("사용자정보가 변경되었습니다.");
+  }
+
+  const callAPI = async() => {
+    setLoading(true);
+    const res=await getDoc(doc(db, 'users', uid));
+    if(res.data()){
+      setForm(res.data());
+    }
+    setLoading(false);
+  }
+
+  useEffect(()=>{
+    callAPI();
+  }, []);
+
+  if(loading) return <h1 className='text-center my-5'>로딩중......</h1>
+  return (
+    <Row className='justify-content-center my-5 mypage'>
+      <Col xs={10} md={8} lg={7}>
+        <Card>
+          <Card.Header>
+            <h3 className='text-center'>마이페이지</h3>
+          </Card.Header>
+          <Card.Body>
+            <div>
+              <ModalPhoto form={form} setForm={setForm} setLoading={setLoading}/>
+              <span className='ms-3'>{email}</span>
+            </div>
+            <form onSubmit={onSubmit}>
+              <InputGroup className='mb-2'>
+                <InputGroup.Text className='px-5'>이름</InputGroup.Text>
+                <Form.Control name="name" value={name} onChange={onChangeForm}/>
+              </InputGroup>
+              <InputGroup className='mb-2'>
+                <InputGroup.Text className='px-5'>전화</InputGroup.Text>
+                <Form.Control name="phone" value={phone} onChange={onChangeForm}/>
+              </InputGroup>
+              <InputGroup className='mb-2'>
+                <InputGroup.Text className='px-5'>주소</InputGroup.Text>
+                <Form.Control name="address1" value={address1} onChange={onChangeForm}/>
+                <ModalAddress setForm={setForm} form={form}/>
+              </InputGroup>
+              <Form.Control name="address2" value={address2} onChange={onChangeForm} placeholder='상세주소'/>
+              <div className='text-center mt-3'>
+                <Button type="submit" className='px-5'>등록</Button>
+                <Button onClick={callAPI} className='px-5 ms-3' variant='secondary'>취소</Button>
+              </div>
+            </form>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  )
+}
+
+export default MyPage
+
